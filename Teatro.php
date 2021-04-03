a0b866161fac5c263363ebdcb772377887117172
CintiaSanchez <cintia.sanchez@est.fi.uncoma.edu.ar>
2021-04-02 21:24:46 -0300

    Entrega de trabajo

---
+++ Teatro.php
@@ -0,0 +1,61 @@
+<?php
+class Teatro{
+//------------propiedades--------------------------
+    private $nombre;
+    private $direccion;
+    private $funcion=array( array("nombre"=>"default1","precio"=>0), array("nombre"=>"default2","precio"=>0), array("nombre"=>"default3","precio"=>0), array("nombre"=>"default4","precio"=>0));
+
+//------------constructor--------------------------
+    /**
+    * @param string $nombre
+    * @param string $direccion
+    */
+    public function __construct($nombre,$direccion){
+        $this->nombre=$nombre;
+        $this->direccion=$direccion;
+    }
+//---------------metodos---------------------------
+    public function getNombre(){
+        return $this->nombre;
+    }
+    public function getDireccion(){
+        return $this->direccion;
+    }
+    /**
+     * @param int $indice
+     */
+    public function getFuncion($indice){
+        return "Nombre: ".$this->funcion[$indice]["nombre"]."\nPrecio: ".$this->funcion[$indice]["precio"];
+    }
+
+    /**
+     * @param string $nombre
+     */
+    public function setNombre($nombre){
+        $this->nombre=$nombre;
+    }
+    /**
+     * @param string $direccion
+     */
+    public function setDireccion($direccion){
+        $this->direccion=$direccion;
+    }
+    /**
+     * @param int $indice
+     * @param string $funcion
+     * @param string $funcion
+     */
+    public function setFuncion($indice,$funcion,$precio){
+        $this->funcion[$indice]["nombre"]=$funcion;
+        $this->funcion[$indice]["precio"]=$precio;
+    }
+
+    public function __toString()
+    {
+        echo "Datos del objeto:\nNombre: ".$this->nombre."\nDireccion: ".$this->direccion."\nFuncion 1: ".$this->getFuncion(0).
+        "\nFuncion 2: ".$this->getFuncion(1)."\nFuncion 3: ".$this->getFuncion(2)."\nFuncion 4: ".$this->getFuncion(3);
+    }
+
+//------------------------------------------------
+}
+?>
---
+++ testTeatro.php
@@ -0,0 +1,127 @@
+<?php
+include "Teatro.php";
+$unTeatro = new Teatro("Barcelona","Av.San Luis 745");
+echo "Un arreglo de la clase \"Teatro\" fue creado\n"; 
+do{
+
+echo "¿Qué acción desea realizar?
+1)Modificar datos
+2)Visualizar datos\n";
+
+
+if(trim(fgets(STDIN)) == 1)
+{
+    $accion = "modificar";
+}
+else
+{
+    $accion = "visualizar";
+}
+
+
+echo "¿Que propiedad desea $accion?
+1)El nombre
+2)La direccion
+3)Los datos de la funcion 1
+4)Los datos de la funcion 2
+5)Los datos de la funcion 3
+6)Los datos de la funcion 4\n";
+
+switch(trim(fgets(STDIN))){
+    case 1:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getNombre()."\n";
+        }
+        else
+        {
+            echo "Indique nuevo nombre del teatro: ";
+            $unTeatro->setNombre(trim(fgets(STDIN)));
+        }
+        break;
+
+    case 2:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getDireccion()."\n";
+        }
+        else
+        {
+            echo "Indique la nueva direccion: ";
+            $unTeatro->setDireccion(trim(fgets(STDIN)));
+        }
+        break;
+
+    case 3:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getFuncion(0)."\n";
+        }
+        else
+        {
+            echo "Indique el nombre de la funcion: ";
+            $funcion = trim(fgets(STDIN));
+            echo "Indique el precio: ";
+            $precio = trim(fgets(STDIN));
+
+            $unTeatro->setFuncion(0,$funcion,$precio);
+        }
+        break;
+
+    case 4:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getFuncion(1)."\n";
+        }
+        else
+        {
+            echo "Indique el nombre de la funcion: ";
+            $funcion = trim(fgets(STDIN));
+            echo "Indique el precio: ";
+            $precio = trim(fgets(STDIN));
+
+            $unTeatro->setFuncion(1,$funcion,$precio);
+        }
+        break;
+        
+    case 5:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getFuncion(2)."\n";
+        }
+        else
+        {
+            echo "Indique el nombre de la funcion: ";
+            $funcion = trim(fgets(STDIN));
+            echo "Indique el precio: ";
+            $precio = trim(fgets(STDIN));
+
+            $unTeatro->setFuncion(2,$funcion,$precio);
+        }
+        break;
+    case 6:
+        if($accion == "visualizar")
+        {
+            echo $unTeatro->getFuncion(3)."\n";
+        }
+        else
+        {
+            echo "Indique el nombre de la funcion: ";
+            $funcion = trim(fgets(STDIN));
+            echo "Indique el precio: ";
+            $precio = trim(fgets(STDIN));
+
+            $unTeatro->setFuncion(3,$funcion,$precio);
+        }
+        break;
+}
+//-------------------------------------------
+
+echo "¿Desea realizar otra accion? si-no\n";
+$r=trim(fgets(STDIN));
+
+}while($r=="si");
+
+//$unTeatro->__toString(); QUITAR COMENTARIO PARA VISUALIZAR TODOS LOS ELEMENTOS DEL OBJETO
+
+?>
